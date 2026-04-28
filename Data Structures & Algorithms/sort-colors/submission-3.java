9db6d6ba98b8bc05d9467d4e80ada2003f24b057
rashmi-crypto <rashmipyne@gmail.com>
2026-04-28 13:19:09 +0530

    Add: sort-colors - submission-3

---
+++ Data Structures & Algorithms/sort-colors/submission-3.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low=0;
+        int current=low;
+        int high=nums.length-1;
+        while(current<=high)
+        {
+            if(nums[current]==0)
+            {
+                swap(nums,current,low);
+                current++;
+                low++;
+            }
+            else if(nums[current]==2)
+            {
+                swap(nums,current,high);
+                high--;
+            }
+            else
+            current++;
+        }
+    }
+    private void swap(int[] nums,int i,int j)
+    {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}

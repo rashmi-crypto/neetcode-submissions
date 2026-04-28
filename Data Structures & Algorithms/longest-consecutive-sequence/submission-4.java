d10164cca0a96ddd0da5c56ec41b36ec64a180e4
rashmi-crypto <rashmipyne@gmail.com>
2026-04-28 13:06:43 +0530

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0)
+        return 0;
+        int max=1;
+        int c=1;
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]==nums[i-1])
+            continue;
+            else if(nums[i]==nums[i-1]+1)
+            c++;
+            else
+            {
+                max=Math.max(max,c);
+                c=1;
+            }
+        }
+        return Math.max(max,c);
+    }
+}

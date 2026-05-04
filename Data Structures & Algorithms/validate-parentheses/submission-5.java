8a331db7bf3da5d3910cf8bf2b928660f54ad241
rashmi-crypto <rashmipyne@gmail.com>
2026-05-04 13:12:49 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        HashMap<Character,Character> map=new HashMap<>();
+        if(s.length()%2!=0)
+        return false;
+        map.put('}','{');
+        map.put(')','(');
+        map.put(']','[');
+        for(char c:s.toCharArray())
+        {
+            if(map.containsKey(c))
+            {
+                if(!stack.isEmpty() && stack.peek()==map.get(c))
+                stack.pop();
+                else
+                return false;
+            }
+            else
+            stack.push(c);
+        }
+        return stack.isEmpty();
+    }
+}
